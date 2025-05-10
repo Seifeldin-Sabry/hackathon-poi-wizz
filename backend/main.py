459115235d58062dea33e1d8e44381541dc54d90
Seifeldin Sabry <93074813+Seifeldin-Sabry@users.noreply.github.com>
2025-05-10 15:37:20 +0200

    setup backend

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,8 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from app.api import index, documents, search
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-# need a change
+app.include_router(index.router)
+app.include_router(documents.router, prefix="/documents")
+app.include_router(search.router, prefix="/search")
